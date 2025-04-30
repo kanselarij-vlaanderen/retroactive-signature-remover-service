83a044f71a16aeacb565de3caaea3f3604877f6f
Sergio Fenoll <sergio@fenoll.be>
2025-04-30 16:50:18 +0200

    feat: initial commit

--- app.js
+++ app.js
@@ -1,9 +1,146 @@
-// see https://github.com/mu-semtech/mu-javascript-template for more info
+import fs from 'fs';
+import readline from 'readline';
+import { app, query, update, errorHandler, sparqlEscapeUri, sparqlEscapeDateTime } from 'mu';
+import { readMuFile } from './lib/file';
+import isFileSigned from './lib/signed-file';
 
-import { app, query, errorHandler } from 'mu';
+class PieceCache {
+  DATE_START_KALEIDOS = new Date("2019-10-02");
+  DATE_SIGNATURE_REMOVER_IN_USE = new Date("2024-04-12");
+  BATCH_SIZE = 100;
 
-app.get('/', function( req, res ) {
-  res.send('Hello mu-javascript-template');
-} );
+  cachePath = '/cache/pieces';
+  lastCreatedPath = '/cache/last_created';
+
+  async getPieceUris() {
+    let uris = [];
+    let lastCreated = null;
+
+    if (fs.existsSync(this.lastCreatedPath)) {
+      const lastCreatedValue = fs.readFileSync(this.lastCreatedPath, { encoding: 'utf-8' });
+      lastCreated = new Date(lastCreatedValue.trim());
+
+      const oldUris = await this._readPiecesFromCache();
+
+      const result = this._getPiecesFromDb(lastCreated);
+      const newUris = result.uris;
+      lastCreated = result.lastCreated ?? lastCreated;
+
+      uris = oldUris.concat(newUris);
+    } else {
+      const result = await this._getPiecesFromDb(this.DATE_START_KALEIDOS);
+      uris = result.uris;
+      lastCreated = result.lastCreated;
+    }
+
+    this._writePiecesToCache(uris, lastCreated);
+
+    return uris;
+  }
+
+  async _readPiecesFromCache() {
+    const uris = [];
+    const stream = fs.createReadStream(this.cachePath);
+    const rl = readline.createInterface({
+      input: stream,
+    });
+
+    for await (const line of rl) {
+      uris.push(line);
+    }
+    return uris;
+  }
+
+  _writePiecesToCache(uris, lastCreated) {
+    if (uris?.length) {
+      const stream = fs.createWriteStream(this.cachePath, { flags: 'a' });
+      for (const uri of uris) {
+        stream.write(`${uri}\n`);
+      }
+      stream.close();
+    }
+
+    if (lastCreated) {
+      fs.writeFileSync(this.lastCreatedPath, lastCreated.toISOString());
+    }
+  }
+
+  async _getPiecesFromDb(startDate) {
+    const paginatedQuery = (startDate) => {
+      return `PREFIX dossier: <https://data.vlaanderen.be/ns/dossier#>
+PREFIX pav: <http://purl.org/pav/>
+PREFIX dct: <http://purl.org/dc/terms/>
+PREFIX sign: <http://mu.semte.ch/vocabularies/ext/handtekenen/>
+PREFIX prov: <http://www.w3.org/ns/prov#>
+
+SELECT DISTINCT ?piece ?file ?created
+WHERE {
+  GRAPH <http://mu.semte.ch/graphs/organizations/kanselarij> {
+    ?piece a dossier:Stuk ;
+      prov:value ?file ;
+      dct:created ?created .
+    FILTER EXISTS { ?serie dossier:Collectie.bestaatUit ?piece }
+    FILTER NOT EXISTS { ?piece sign:ongetekendStuk ?unsignedPiece }
+
+    FILTER (?created > ${sparqlEscapeDateTime(startDate)})
+    FILTER (?created < ${sparqlEscapeDateTime(this.DATE_SIGNATURE_REMOVER_IN_USE)})
+  }
+}
+ORDER BY ?created
+LIMIT ${this.BATCH_SIZE}`;
+    };
+
+    let uris = [];
+    let lastCreated = startDate ?? this.DATE_START_KALEIDOS;
+    do {
+      const queryString = paginatedQuery(lastCreated);
+      const response = await query(queryString, { sudo: true });
+
+      if (!response?.results?.bindings?.length) {
+        break;
+      }
+
+      const bindings = response.results.bindings;
+
+
+      uris = uris.concat(bindings.map((b) => b["file"]["value"]));
+      lastCreated = new Date(bindings[bindings.length - 1]["created"]["value"]);
+    } while (true);
+
+    return { uris, lastCreated };
+  }
+}
+
+const pieceCache = new PieceCache();
+app.post('/', async function (req, res) {
+  const uris = await pieceCache.getPieceUris();
+
+  const signedUris = [];
+  for (const uri of uris) {
+    // Find out which files are signed
+    try {
+      const pdfBytes = readMuFile(uri);
+      if (isFileSigned(pdfBytes)) {
+        signedUris.push(uri);
+      }
+    } catch (e) {
+      continue;
+    }
+  }
+
+  console.debug(signedUris);
+
+
+  if (signedUris?.length) {
+    const stream = fs.createWriteStream('/cache/signed-uris', { flags: 'a' });
+    for (const uri of signedUris) {
+      stream.write(`${uri}\n`);
+    }
+    stream.close();
+  }
+
+  res.sendStatus(204);
+});
+
 
 app.use(errorHandler);
